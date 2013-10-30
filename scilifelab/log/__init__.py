c7003e83500f98ddd219997a17f33f019e8e4b3d
Guillermo Carrasco <guille.ch.88@gmail.com>
2013-10-30 17:10:48 +0100

    Add standalone option to create handlers if not running pm. Otherwise do NOT create any handler and just return an instance of Logger

--- scilifelab/log/__init__.py
+++ scilifelab/log/__init__.py
@@ -13,7 +13,7 @@ try:
 except ImportError:
     import bcbio.pipeline.config_loader as cl
 
-def minimal_logger(namespace, debug=False):
+def minimal_logger(namespace, extra_fields=None, standalone=False, debug=False):
     """Make and return a minimal console logger.
 
     NOTE: this does apparently *not* work with logbook as I first thought, and
@@ -27,35 +27,37 @@ def minimal_logger(namespace, debug=False):
     """
     config = cf.load_config()
     log = logbook.Logger(namespace, level=logbook.INFO)
-    s_h = logbook.StreamHandler(sys.stdout, level = logbook.INFO, bubble=True)
-    log.handlers.append(s_h)
-    try:
-        host = config.get('log', 'redis_host')
-        port = config.getint('log', 'redis_port')
-        key = config.get('log', 'redis_key')
-        password = config.get('log', 'redis_password')
-        extra_fields = {"program": "pm",
-                        "command": namespace}
-        r_h = RedisHandler(host=host, port=port, key=key, password=password,
-                extra_fields=extra_fields, level=logbook.INFO, bubble=True)
-        log.handlers.append(r_h)
-    except:
-        log.warn('Not loading RedisHandler')
-        pass
-
-    # FIX ME: really don't want to hard check sys.argv like this but
-    # can't figure any better way get logging started (only for debug)
-    # before the app logging is setup. Besides, this will fail for
-    # tests since sys.argv will consist of the test call arguments.
-    if '--debug' in sys.argv or debug:
+    if standalone:
+        s_h = logbook.StreamHandler(sys.stdout, level = logbook.INFO, bubble=True)
+        log.handlers.append(s_h)
         try:
-            #If there was any problem loading the RedisHandler, at this point
-            #the variable r_h will not exist
-            r_h.level = logbook.DEBUG
-        except UnboundLocalError:
+            host = config.get('log', 'redis_host')
+            port = config.getint('log', 'redis_port')
+            key = config.get('log', 'redis_key')
+            password = config.get('log', 'redis_password')
+            if extra_fields is None:
+                extra_fields = {"program": "pm",
+                                "command": namespace}
+            r_h = RedisHandler(host=host, port=port, key=key, password=password,
+                    extra_fields=extra_fields, level=logbook.INFO, bubble=True)
+            log.handlers.append(r_h)
+        except:
+            log.warn('Not loading RedisHandler')
             pass
-        s_h.level = logbook.DEBUG
-        log.level = logbook.DEBUG
+
+        # FIX ME: really don't want to hard check sys.argv like this but
+        # can't figure any better way get logging started (only for debug)
+        # before the app logging is setup. Besides, this will fail for
+        # tests since sys.argv will consist of the test call arguments.
+        if '--debug' in sys.argv or debug:
+            try:
+                #If there was any problem loading the RedisHandler, at this point
+                #the variable r_h will not exist
+                r_h.level = logbook.DEBUG
+            except UnboundLocalError:
+                pass
+            s_h.level = logbook.DEBUG
+            log.level = logbook.DEBUG
 
     return log
 
